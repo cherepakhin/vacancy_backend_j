5d54afddbaef2b39de7d54667f29485aad7a6062
Черепахин Василий <vasi.che@gmail.com>
2024-05-21 17:23:49 +0500

    add V1_003__load_test_companies.sql, V1_004__load_test_vacancies.sql

---
+++ src/main/resources/db/migration/V1_003__load_test_companies.sql
@@ -0,0 +1,2 @@
+insert into company(n, name) values (1,'Company 1');
+insert into company(n, name) values (2,'Company 2');
---
+++ src/main/resources/db/migration/V1_004__load_test_vacancies.sql
@@ -0,0 +1,5 @@
+insert into vacancy(n, title, description, company_n)
+    values (1,'Vacancy 1 Company 1', 'Description Vacancy 1 Company 1',1),
+    (2,'Vacancy 2 Company 1', 'Description Vacancy 2 Company 1',1),
+    (3,'Vacancy 1 Company 2', 'Description Vacancy 1 Company 2',2),
+    (4,'Vacancy 2 Company 2', 'Description Vacancy 2 Company 2',2);
