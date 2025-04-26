b80ee0a628cae60cd8d6728d7475e892b076b1c1
Черепахин Василий <vasi.che@gmail.com>
2025-04-26 10:25:45 +0500

    fix VacancyServiceImplIntegrationTest.findByName() . Fix 'Comment 1'. All test OK.

--- src/test/java/ru/perm/v/vacancy_j/service/impl/VacancyServiceImplIntegrationTest.java
+++ src/test/java/ru/perm/v/vacancy_j/service/impl/VacancyServiceImplIntegrationTest.java
@@ -44,7 +44,8 @@ public class VacancyServiceImplIntegrationTest {
         assertEquals(1, vacancies.size());
 
         CompanyDto companyDto = new CompanyDto(1L,"Company 1");
-        VacancyDto vacancy = new VacancyDto(1L, "Vacancy 1 Company 1", "Description Vacancy 1 Company 1", companyDto, "TODO", "");
+        VacancyDto vacancy = new VacancyDto(1L, "Vacancy 1 Company 1",
+                "Description Vacancy 1 Company 1", companyDto, "Link 1", "Comment 1");
         assertEquals(vacancy,vacancies.get(0));
     }
 
