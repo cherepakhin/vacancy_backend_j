7596dae62368cbe782df49fe69dbdb3d9ceec14b
Черепахин Василий <vasi.che@gmail.com>
2025-04-29 14:41:35 +0500

    add ValidatorVacancyDtoTest()

--- src/test/java/ru/perm/v/vacancy_j/rest/validator/ValidatorVacancyDtoTest.java
+++ src/test/java/ru/perm/v/vacancy_j/rest/validator/ValidatorVacancyDtoTest.java
@@ -1,17 +1,15 @@
 package ru.perm.v.vacancy_j.rest.validator;
 
 import org.junit.jupiter.api.Test;
-import ru.perm.v.vacancy_j.dto.CompanyDto;
 import ru.perm.v.vacancy_j.dto.VacancyDto;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ValidatorVacancyDtoTest {
     @Test
-    void validateShortTitle() {
+    void validateEmptyTitle() {
         ValidatorVacancyDto validator = new ValidatorVacancyDto();
         VacancyDto dto = new VacancyDto();
         dto.setTitle("");
@@ -19,6 +17,26 @@ public class ValidatorVacancyDtoTest {
         List<String> errors = validator.validate(dto);
 
         assertEquals(2, errors.size());
-        assertEquals("field: title, error: не должно быть пустым\n", errors.get(0));
+        Integer countErrors = 0;
+        for (String err : errors) {
+            if (err.equals("field: title, error: не должно быть пустым\n") ||
+                    err.equals("field: title, error: Длина должна быть больше 5 символов.\n")
+            ) {
+                countErrors++;
+            }
+        }
+        assertEquals(2, countErrors);
+    }
+
+    @Test
+    void validateShortTitle() {
+        ValidatorVacancyDto validator = new ValidatorVacancyDto();
+        VacancyDto dto = new VacancyDto();
+        dto.setTitle("123");
+
+        List<String> errors = validator.validate(dto);
+
+        assertEquals(1, errors.size());
+        assertEquals("field: title, error: Длина должна быть больше 5 символов.\n", errors.get(0));
     }
 }
