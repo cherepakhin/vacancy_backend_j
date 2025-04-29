fe46dae1b148d33e68950bb8dbb0753c780ff8b2
Черепахин Василий <vasi.che@gmail.com>
2025-04-29 09:50:31 +0500

    добавлен Logger в EchoRest.java

--- src/main/java/ru/perm/v/vacancy_j/rest/EchoRest.java
+++ src/main/java/ru/perm/v/vacancy_j/rest/EchoRest.java
@@ -1,13 +1,17 @@
 package ru.perm.v.vacancy_j.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/echo")
 @CrossOrigin(origins = "*")
 public class EchoRest {
+    Logger log = LoggerFactory.getLogger(EchoRest.class);
     @GetMapping("/{message}")
     public String echo(@PathVariable String message) {
+        log.info("Echo message: {}", message);
         return message;
     }
 }
