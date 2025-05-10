91243e2362eb410af818724a2df1c75b90fa8b97
Черепахин Василий <vasi.che@gmail.com>
2025-05-10 12:05:33 +0500

    add V1_010__add_v_perm_ru.sql

---
+++ src/main/resources/db/migration/V1_010__add_v_perm_ru.sql
@@ -0,0 +1,2 @@
+update vacancy set link='https://v.perm.ru/', status='in_work' where n=1;
+COMMIT;
--- src/main/resources/db/migration/V1_010_add_v_perm_ru.sql
+++
@@ -1,2 +0,0 @@
-update set link="https://v.perm.ru/", status="in_work" vacancy where n=1;
-COMMIT;
