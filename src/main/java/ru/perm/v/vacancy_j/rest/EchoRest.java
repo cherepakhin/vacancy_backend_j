e758c561d0516045266c91582b9af82ebc10f8f2
Черепахин Василий <vasi.che@gmail.com>
2025-05-10 15:30:52 +0500

    // Tag для группировки (на пример все GET запросы в одной секции)

--- src/main/java/ru/perm/v/vacancy_j/rest/EchoRest.java
+++ src/main/java/ru/perm/v/vacancy_j/rest/EchoRest.java
@@ -1,19 +1,17 @@
 package ru.perm.v.vacancy_j.rest;
 
-import io.swagger.v3.oas.annotations.Parameter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-//import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameter;
 
 @RestController
 @RequestMapping("/echo")
 @CrossOrigin(origins = "*")
-@Tag(name = "Echo REST controller", description = "Controller for Echo test")
+@Tag(name = "Echo REST controller", description = "Controller for Echo test") // Tag для группировки (на пример все GET запросы в одной секции)
 public class EchoRest {
     Logger log = LoggerFactory.getLogger(EchoRest.class);
     @GetMapping("/{message}")
