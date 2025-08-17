81e825dcf6e33178dee4bce946962932b22cdf63
Черепахин Василий <vasi.che@gmail.com>
2025-08-17 23:00:58 +0500

    add MapperTest.convertJsonArrayToList()

---
+++ src/test/java/ru/perm/v/vacancy_j/example/MapperTest.java
@@ -0,0 +1,25 @@
+package ru.perm.v.vacancy_j.example;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//тест простой конвертации JSON в объект
+public class MapperTest {
+
+    @Test
+    void convertJsonArrayToList() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonCarArray =
+                "[{ \"color\" : \"Black\", \"type\" : \"BMW\" }, { \"color\" : \"Red\", \"type\" : \"FIAT\" }]";
+        List<Car> listCar = mapper.readValue(jsonCarArray, new TypeReference<>() {
+        });
+
+        assertEquals(2, listCar.size());
+    }
+}
