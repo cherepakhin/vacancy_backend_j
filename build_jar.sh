8ac0963d204ae67e4572fbd4fd1adba153f4911e
vasi <vasi.che@gmail.com>
2025-11-18 18:31:01 +0500

    fix for java17 in run_jar.sh, build_jar.sh

--- build_jar.sh
+++ build_jar.sh
@@ -1,1 +1,2 @@
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 ./gradlew bootJar
--- run_jar.sh
+++ run_jar.sh
@@ -1,3 +1,5 @@
 # echo $JAVA_HOME
 # /usr/lib/jvm/java-17-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+#/usr/lib/jvm/java-17-openjdk-amd64/bin/java  -jar build/libs/vacancy_backend-0.0.1-SNAPSHOT.jar
 /usr/lib/jvm/java-17-openjdk-amd64/bin/java -Dserver.port=8090 -jar build/libs/vacancy_backend-0.0.1-SNAPSHOT.jar
