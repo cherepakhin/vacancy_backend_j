f074f76cfb02945bbf507d400ca17a3cd43b5c6f
vasi <vasi.che@gmail.com>
2025-11-25 11:10:42 +0500

    Тест использования конфигурации списка сайтов

--- src/test/java/ru/perm/v/vacancy_j/conf/ExtListSiteBeanUseTest.java
+++ src/test/java/ru/perm/v/vacancy_j/conf/ExtListSiteBeanUseTest.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 @SpringBootTest
 public class ExtListSiteBeanUseTest {
+    // Так можно получить список сайтов из конфигурации как Bean
     @Autowired
     private List<ExtSiteDto> listExtSiteDto;
 
